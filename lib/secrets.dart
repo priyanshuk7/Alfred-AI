e72a282d690f1a18b7114ed99fdb0b1c2ccced0c
ACER <priyanshu.katariya2003@gmail.com>
2024-07-09 20:34:17 +0530

    first

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,1 +1,2 @@
-const openAiAPIKey = 'sk-proj-gOdLXSCr8Y00UMBSJv43T3BlbkFJwb4msSeN6FVxi49VLzZ1';
+//const openAiAPIKey = 'sk-proj-gOdLXSCr8Y00UMBSJv43T3BlbkFJwb4msSeN6FVxi49VLzZ1';
+const openAiAPIKey = '#';
